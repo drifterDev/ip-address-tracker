6f5e6e88f846595115ffa537697327e64b4c22d9
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-09-07 14:06:33 -0500

    Arreglando otros detalles

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,24 +3,20 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: 'airbnb-base',
+  extends: "eslint:recommended",
   overrides: [
     {
       env: {
         node: true,
       },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
+      files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: 'script',
+        sourceType: "script",
       },
     },
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
   },
-  rules: {
-  },
+  rules: {},
 };
--- src/script.js
+++ src/script.js
@@ -4,17 +4,17 @@
 // Este código se proporciona bajo la Licencia MIT.
 // Para más información, consulta el archivo LICENSE en la raíz del repositorio.
 
-'use strict';
+/* eslint-disable no-undef */
+
 const labelIp = document.getElementById('labelIp');
 const labelLocation = document.getElementById('labelLocation');
 const labelTime = document.getElementById('labelTime');
 const labelISP = document.getElementById('labelISP');
 const btn = document.getElementById('btn');
-var map;
+let map;
 
 function esDireccionIPValida(valor) {
-  const ipRegex =
-    /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+  const ipRegex = /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
   return ipRegex.test(valor);
 }
 
@@ -22,15 +22,13 @@ async function obtenerIP() {
   try {
     const response = await fetch('https://api.ipify.org?format=json');
     if (!response.ok) {
-      throw new Error(
-        'Error en la petición. Código de estado: ' + response.status
-      );
+      throw new Error(`Error en la petición. Código de estado: ${response.status}`);
     }
     const data = await response.json();
-    const ip = data.ip;
+    const ip = await data.ip;
     return ip;
   } catch (error) {
-    console.error('Error al obtener la dirección IP:', error.message);
+    console.log('Error al obtener la dirección IP: ', error.message);
     throw error;
   }
 }
@@ -45,19 +43,19 @@ async function obtenerLatitudLongitudDesdeIP(ip) {
       );
     }
     let data = await response.json();
-    let numeros = await data.loc.split(',');
-    let latitud = await parseInt(numeros[0]);
-    let longitud = await parseInt(numeros[1]);
-
-    labelIp.textContent = await data.ip;
-    labelLocation.textContent = await (data.country + ' ' + data.city);
+    let nums = await data.loc.split(',');
+    let lat = await parseInt(nums[0]);
+    let lon = await parseInt(nums[1]);
     let textoTime = await data.timezone;
-    labelTime.textContent = textoTime.replace('_', ' ');
     let palabras = await data.org.split(' ');
     palabras.shift();
-    labelISP.textContent = await palabras.join(' ');
 
-    return { latitud, longitud };
+    labelIp.textContent = await data.ip;
+    labelLocation.textContent = await (data.region + ', ' + data.city);
+    labelTime.textContent = textoTime.replace('_', ' ');
+    labelISP.textContent = await palabras.join(' ');
+    
+    return { lat, lon };
   } catch (error) {
     console.error('Error al obtener la latitud y longitud:', error.message);
     throw error;
